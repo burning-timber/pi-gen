fd5585b424f561c513664e0cc0450d754ad036f9
Joe Cooter <joe@burningtimber.com>
2021-04-09 15:07:36 -0400

    auto-gen passwords

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -e
 
+user_passwd=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16})
+root_passwd=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16})
+
+cat <<EOF > /pi-gen/deploy/users
+${user_passwd}
+${root_passwd}
+EOF
+
 install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install -m 644 files/noclear.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/noclear.conf"
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
@@ -8,8 +16,8 @@ on_chroot << EOF
 if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
 	adduser --disabled-password --gecos "" ${FIRST_USER_NAME}
 fi
-echo "${FIRST_USER_NAME}:${FIRST_USER_PASS}" | chpasswd
-echo "root:root" | chpasswd
+echo "${FIRST_USER_NAME}:${user_passwd}" | chpasswd
+echo "root:${root_passwd}" | chpasswd
 EOF
 
 
